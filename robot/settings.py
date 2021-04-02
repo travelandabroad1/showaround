79a4c52f9d2fa6ab16d30a5a431c3c9062643801
travelandabroad1 <81826065+travelandabroad1@users.noreply.github.com>
2021-04-02 20:11:12 +0200

    Update settings.py

--- robot/settings.py
+++ robot/settings.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 ROBOT_FOLDER = Path(os.path.dirname(os.path.realpath(__file__))).parent
 
+RETRY_TIMES = 3
+
 """Folder to store Chrome Driver"""
 CHROMEDRIVER_PATH = os.path.join(ROBOT_FOLDER, "Driver")
 
@@ -13,6 +15,11 @@ EMAIL_PASSWORD = None
 """Outgoing email settings"""
 EMAIL_SMTP_SERVER = None
 EMAIL_SMTP_POST = None
+
+
+LOGIN_MAIL = "info@travelandabroad.com"
+LOGIN_PASSWORD = "Gonzalo01!"
+
 
 """Incoming email settings"""
 EMAIL_IMAP_SERVER = None
