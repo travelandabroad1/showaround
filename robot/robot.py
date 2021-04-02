bda4d4effb37eaf67c858bf6d748212cca13891d
travelandabroad1 <81826065+travelandabroad1@users.noreply.github.com>
2021-04-02 20:10:21 +0200

    Update robot.py

--- robot/robot.py
+++ robot/robot.py
@@ -1,6 +1,11 @@
-from iBott.robot_activities import Robot, RobotException, Robotmethod, get_all_Methods, get_instances
+import os
+import time
+import random
+import pandas as pd
+from iBott.robot_activities import Robot, RobotException, Robotmethod, get_all_Methods, get_instances, Queue
 from iBott.browser_activities import ChromeBrowser
 import robot.settings as settings
+from navigation.showaround import Showaround
 
 
 class Main(Robot):
@@ -31,24 +36,55 @@ class Main(Robot):
 
         self.browser = ChromeBrowser()
         self.browser.open()
+        self.browser.maximize_window()
 
-        pass
+        # init QueueId
+
+        city_list = pd.read_excel(os.path.join(settings.ROBOT_FOLDER, "city_list.xlsx"), header=0)
+        city_list = city_list["Cities"].tolist()
+        city_list = random.sample(city_list, len(city_list))
+
+        self.queue = Queue(robotId=self.robotId, url=self.url, token=self.token, queueName="Showaround Bot")
+        self.queue.setRetryTimes(10)
+        for city in city_list:
+            self.queue.createItem({"city": city})
+
+        #Init showaaround classes
+        self.showaround = Showaround(self.browser, self)
+
+
+    @Robotmethod
+    def login(self):
+        try:
+            self.showaround.login()
+        except:
+            raise SystemException("Login fail", "login")
 
     @Robotmethod
     def process(self):
         """Run robot process"""
-
+        while True:
+            city = self.queue.getNextItem()
+            if city is None:
+                break
+            try:
+                self.showaround.apply(city)
+                city.setItemAsOk()
+            except Exception as e:
+                raise SystemException
+                city.setItemAsFail()
 
     @Robotmethod
     def end(self):
         """Finish robot execution, cleanup environment, close applications and send reports"""
 
+        self.browser.close()
 
 
 class BusinessException(RobotException):
     """Manage Exceptions Caused by business errors"""
 
-    def _init__(self,  message, action):
+    def _init__(self, message, action):
         super().__init__(get_instances(Main), action)
         self.action = action
         self.message = message
